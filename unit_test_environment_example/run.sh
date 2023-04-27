6e867a979b0c65c8d5d052211ca03c6d8424fcc7
Cappuccinomild <rkdquddh1208@naver.com>
2023-04-27 21:12:38 +0900

    .

--- unit_test_environment_example/run.sh
+++ unit_test_environment_example/run.sh
@@ -1,16 +1,29 @@
 python3 mk_driver.py $1 > test_driver.c 
 gcc -o a.out $2 test_driver.c -fprofile-arcs -ftest-coverage
-./a.out
+
+echo "Driver Test Result" >> report.txt
+./a.out >> report.txt
+
+echo "" >> report.txt
+echo "" >> report.txt
+
 gcov -b -c a-test_driver.c >> report.txt
+
+echo "" >> report.txt
+echo "" >> report.txt
+
 gcov -b -c a-$2 >> report.txt
 
-echo "driver report" >> report.txt
+echo "" >> report.txt
+echo "" >> report.txt
+
+echo "Driver Report" >> report.txt
 echo "" >> report.txt
 echo "" >> report.txt
 
 cat test_driver.c.gcov >> report.txt
 
-echo "unit report" >> report.txt
+echo "Unit Report" >> report.txt
 echo "" >> report.txt
 echo "" >> report.txt
 
